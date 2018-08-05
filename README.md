ec72400d2b26207b143e0ca4ae44962e82f2699e
Ayra Hikari <36266025+AyraHikari@users.noreply.github.com>
2018-08-05 22:21:44 +0700

    Initial v1.0

---
+++ README.md
@@ -0,0 +1,20 @@
+# Simsimi Telegram Bot
+This is example how simsimi API work on python for telegram bot
+
+## How to use
+First edit simsimi.py using text editor.
+Place your API Telegram bot to `TOKEN` and Simsimi API to `SimsimiKey`
+Then open terminal/cmd, install requirements first using `pip install -r requirements.txt`
+Last, run bot using `python simsimi.py`, if it say `Running bot success!` it running successfully. Test bot by yourself.
+
+### How to work on telegram groups
+Edit simsimi.py using text editor.
+See line 31, replace `Filters.private` with `Filters.group`
+Note: using `Filters.group` will reply every send text on your group.
+More info on `Filters`, [see here](http://python-telegram-bot.readthedocs.io/en/latest/telegram.ext.filters.html)
+
+## Screenshot
+![Simsimi bot telegram](http://kentod.heliohost.org/kentod.heliohost.org/ayra/simsimibot.jpg "Simsimi Telegram Bot")
+
+## Q&A
+If you need help, join [@NusantaraDevs](https://t.me/NusantaraDevs) and ask me in group, not PM. i will answer you as long as possible.
---
+++ simsimi.py
@@ -0,0 +1,43 @@
+import telegram
+import requests
+import logging
+
+from telegram.ext import Updater
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+
+TOKEN = "YOUR BOT TOKEN FROM @BotFather"
+SimsimiKey = "YOUR SIMSIMI API TOKEN"
+
+bot = telegram.Bot(token=TOKEN)
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+
+def SimSimi(bot, update):
+	message = update.effective_message
+	thetext = message.text
+	try:
+		URL = "http://sandbox.api.simsimi.com/request.p?key={}&lc=id&ft=1.0&text={}".format(SimsimiKey, thetext)
+		r = requests.get(url = URL)
+		data = r.json()
+		if data['result'] == 509:
+			text = data['msg']
+		else:
+			text = data['response']
+		update.effective_message.reply_text(text)
+	except:
+		update.effective_message.reply_text("Error!")
+
+simsimi_handler = MessageHandler(Filters.private, SimSimi)
+"""
+For only private chat response, use Filters.private
+For any text exclude image caption or other, use Filters.text
+For only private and text only, use like this (Filters.private | Filters.text)
+For more info and any filters, see http://python-telegram-bot.readthedocs.io/en/latest/telegram.ext.filters.html#module-telegram.ext.filters
+"""
+
+dispatcher.add_handler(simsimi_handler)
+
+__log__ = logging.getLogger()
+__log__.warning("Running bot success!")
+updater.start_polling()
